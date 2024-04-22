7979bdb4588f0912346bcc7988d4983933cf45e7
vardi <vardii.rodrigues.com>
2024-04-22 11:12:34 -0400

    define custom configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'hsl(14, 45%, 36%)',
+        secondary: 'hsl(332, 51%, 32%)',
+        'roseWhite': 'hsl(330, 100%, 98%)',
+        'eggShell': 'hsl(30, 54%, 90%)',
+        'lightGrey': 'hsl(30, 18%, 87%)',
+        'wengeBrown': 'hsl(30, 10%, 34%)',
+        'darkCharcoal': 'hsl(24, 5%, 18%)',
+      },
+      fontFamily: {
+        young: ["Young Serif", serif],
+        outfit: ["Outfit", sans - serif]
+      }
+    },
   },
   plugins: [],
 }
